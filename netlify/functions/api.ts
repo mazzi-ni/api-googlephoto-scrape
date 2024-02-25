b14c92ab1f1590fed4f56da0bd7652376df725e9
andreamazzini3 <a.mazzini3@campus.unimib.it>
2024-02-25 23:36:42 +0100

    try to fix typescript

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,14 +1,37 @@
-// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
-
 import express, { Router } from "express";
 import serverless from "serverless-http";
+import axios from 'axios';
 
 const api = express();
-
 const router = Router();
+const regex = /\["(https:\/\/lh3\.googleusercontent\.com\/[a-zA-Z0-9\-_\/]*)"/g
+
+function extractPhotos(content: string): string[] {
+  const links = new Set<string>();
+  let match: RegExpExecArray | null;
+  while (match = regex.exec(content)) {
+    links.add(match[1]);
+  }
+  return Array.from(links);
+}
+
+async function getAlbum(id: string): Promise<string[]> {
+  const response = await axios.get(`https://photos.app.goo.gl/${id}`);
+  // console.log(response.data);
+
+  return extractPhotos(response.data);
+}
+
+
 router.get("/hello", (req, res) => res.send("Hello World!"));
+router.get('/:id', (req, res) => res.send(req))
+  // try {
+  //   // const results = await getAlbum(req);
+  //   // return res.json(results);
+  // } catch (e) {
+  //   return res.status(500).send();
+  // }
 
 api.use("/api/", router);
-
 export const handler = serverless(api);
 
